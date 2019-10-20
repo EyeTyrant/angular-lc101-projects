2df42785fb64e6f99fb09dc41e90d1223037846b
EyeTyrant <1414jg@gmail.com>
2019-10-20 14:39:06 -0500

    Angular Lesson1 Exercise Part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'fav-photos',
-  templateUrl: './fav-photos.component.html',
-  styleUrls: ['./fav-photos.component.css']
+  selector: "fav-photos",
+  templateUrl: "./fav-photos.component.html",
+  styleUrls: ["./fav-photos.component.css"]
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = "My Favorite Photos";
+  image1 = "https://unaffiliatedcritic.com/wp-content/uploads/2018/07/THE-MANCHURIAN-CANDIDATE-1962.png";
+  image2 = "https://www.sheknows.com/wp-content/uploads/2018/08/gtvfrowmmmqqcf4omncn.jpeg";
+  image3 = "https://i.ebayimg.com/images/g/Z60AAOSwGWJcPgJ7/s-l300.jpg";
+  image4 =
+    "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2016%2F12%2Ffrozone.jpg&w=400&c=sc&poi=face&q=85";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

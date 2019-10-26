55384baaf49c09a5e4d06ac21e8658549dd0527f
EyeTyrant <1414jg@gmail.com>
2019-10-26 11:21:12 -0500

    Angular Part 3 examples complete

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'buttons',
-   templateUrl: './buttons.component.html',
-   styleUrls: ['./buttons.component.css']
+  selector: "buttons",
+  templateUrl: "./buttons.component.html",
+  styleUrls: ["./buttons.component.css"]
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+  buttonHeading: string = "Buttons";
+  inactive: boolean = false;
+  gInactive: boolean = false;
+  sInactive: boolean = false;
+  cInactive: boolean = false;
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() { }
+  ngOnInit() {}
 
+  activate() {
+    this.gInactive = false;
+    this.sInactive = false;
+    this.cInactive = false;
+  }
 }
